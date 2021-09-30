4376881d308fe795dfed1f1f0c144151cbf3b5a7
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2021-09-29 23:09:43 -0400

    Dead end, stopping point for tonight

--- Optional3/Optionals.java
+++ Optional3/Optionals.java
@@ -1,38 +1,55 @@
 package Optional3;
 
 import java.util.NoSuchElementException;
-import java.util.Scanner;
+// import java.util.Scanner;
+import java.util.Optional;
 
 import Service3.OptionalService;
+import Model1.Boat;
 
 public class Optionals {
     
-    private Scanner scanner = new Scanner(System.in);
+    // private Scanner scanner = new Scanner(System.in);
     private OptionalService service = new OptionalService();
+    private Boat boat = new Boat();
     public static void main(String[] args) {
 
-        new Optionals().run();
+        // new Optionals().run();
+        new Optionals().boatsMethodA();
     }
 
-    private void run() {
-        boolean done = false;
+    public Boat boatsMethodA(Optional<Boat> optionalBoat) {
+        String search = optionalBoat;
 
-        while (!done) {
-            System.out.print("Please enter a String: ");
-            String search = scanner.nextLine();
-
-            if (search.isEmpty()) {
-                done = true;
-            }
-            else {
-                try {
-                String found = service.find(search);
-                System.out.println("It seems you entered " + found + ".");
-                }
-                catch (NoSuchElementException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
+        try {
+            String found = service.find(search);
+            System.out.println("We found " + found + ". Yay, it works!");
+        }
+        catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
         }
     }
+
+    // private void run() {
+        // // This section is from this week's video. Use it to find a way to make the assignment work.
+        // boolean done = false;
+
+        // while (!done) {
+        //     System.out.print("Please enter a String: ");
+        //     String search = scanner.nextLine();
+
+        //     if (search.isEmpty()) {
+        //         done = true;
+        //     }
+        //     else {
+        //         try {
+        //         String found = service.find(search);
+        //         System.out.println("It seems you entered " + found + ".");
+        //         }
+        //         catch (NoSuchElementException e) {
+        //             System.out.println(e.getMessage());
+        //         }
+        //     }
+        // }
+    // }
 }
