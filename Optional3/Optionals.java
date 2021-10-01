46f616c764ba9f9d04cc138814a59fb6730d6408
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2021-09-30 20:48:59 -0400

    Last attempt for tonight
    Multiple attempts being made today to replace the Scanner input from the videos with my SortDao List of boats. No luck, need to attempt again tomorrow.

--- Optional3/Optionals.java
+++ Optional3/Optionals.java
@@ -3,31 +3,29 @@ package Optional3;
 import java.util.NoSuchElementException;
 // import java.util.Scanner;
 import java.util.Optional;
+import java.util.List;
 
 import Service3.OptionalService;
 import Model1.Boat;
+import Dao1.SortDao;
 
 public class Optionals {
     
     // private Scanner scanner = new Scanner(System.in);
     private OptionalService service = new OptionalService();
-    private Boat boat = new Boat();
+    private static SortDao sortDao = new SortDao();
+
     public static void main(String[] args) {
 
         // new Optionals().run();
-        new Optionals().boatsMethodA();
+        //The below line is my attempt to pass through the List of boats into the optional.
+        //Attempt is not working. Hard stuck here.
+        System.out.println(boatsMethodA(getBoats()));
     }
 
-    public Boat boatsMethodA(Optional<Boat> optionalBoat) {
-        String search = optionalBoat;
-
-        try {
-            String found = service.find(search);
-            System.out.println("We found " + found + ". Yay, it works!");
-        }
-        catch (NoSuchElementException e) {
-            System.out.println(e.getMessage());
-        }
+    public static List<Boat> getBoats() {
+        List<Boat> boats = sortDao.getBoats();
+        return boats;
     }
 
     // private void run() {
@@ -52,4 +50,21 @@ public class Optionals {
         //     }
         // }
     // }
+
+    public Boat boatsMethodA(Optional<Boat> optionalBoat) {
+        //Trying to create a for loop here that will run each String of boat through the service.
+        //Unsucessful so far.
+        for (int i = 0; i < optionalBoat.length() - 1; i++) {
+            String search = optionalBoat[i];
+            try {
+                String found = service.find(search);
+                System.out.println("We found " + found + ". Yay, it works!");
+            }
+            catch (NoSuchElementException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return optionalBoat;
+    }
+
 }
